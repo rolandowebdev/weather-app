b52682198aef731334f2fbabc3a7650e1e2deb52
RolandoPranataAR <rolandopranata@gmail.com>
2022-08-07 10:20:02 +0700

    chore(v1): create location & search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Search from './components/search/Search';
+import Location from './components/location/Location';
+
 function App() {
   return (
-    <div>
-      <h1 className='text-6xl text-rose-600 font-bold text-center'>
-        Weather App
-      </h1>
+    <div className='z-10 max-w-lg py-8 mx-auto '>
+      <div className='flex items-center w-full gap-3 '>
+        <Search />
+        <Location />
+      </div>
     </div>
   );
 }
